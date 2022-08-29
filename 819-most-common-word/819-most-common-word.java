770cbbb81fa702807280b2a3fb5f9edd1fea981e
WenZhang <90015869+WenZhang-Vivien@users.noreply.github.com>
2022-08-29 18:57:14 -0400

    Time: 304 ms (12.40%), Space: 44.6 MB (31.96%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -6,33 +6,37 @@ class Solution {
         String[] paragraphStr = normalizedStr.split("\\s+");
         HashMap<String, Integer> wordsHash= new HashMap<String, Integer>();
         for (String word : paragraphStr){
-            if(wordsHash.containsKey(word)){
-                int count = wordsHash.get(word);
-                wordsHash.put(word, count+1);
-            } else{
-                wordsHash.put(word, 1);
+            if (!Arrays.asList(banned).contains(word)){
+                if(wordsHash.containsKey(word)){
+                    int count = wordsHash.get(word);
+                    wordsHash.put(word, count+1);
+                } else{
+                    wordsHash.put(word, 1);
+                }
             }
         }
         
-        List<Map.Entry<String, Integer>> list = new ArrayList<HashMap.Entry<String, Integer>>(wordsHash.entrySet());
+        return Collections.max(wordsHash.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+//         List<Map.Entry<String, Integer>> list = new ArrayList<HashMap.Entry<String, Integer>>(wordsHash.entrySet());
         
         
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
-            public int compare(Map.Entry<String, Integer> map1, Map.Entry<String, Integer> map2){
-                return map2.getValue().compareTo(map1.getValue());
-            }
-        });
+//         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+//             public int compare(Map.Entry<String, Integer> map1, Map.Entry<String, Integer> map2){
+//                 return map2.getValue().compareTo(map1.getValue());
+//             }
+//         });
         
-        String words = "";
-        for (int i = 0 ; i < list.size(); i++){
-            String a = list.get(i).getKey();
-            if (!Arrays.asList(banned).contains(a)){
-                words = a;
-                break;
-            }
-        }
+//         String words = "";
+//         for (int i = 0 ; i < list.size(); i++){
+//             String a = list.get(i).getKey();
+//             if (!Arrays.asList(banned).contains(a)){
+//                 words = a;
+//                 break;
+//             }
+//         }
         
-        return words;    
+//         return words;    
         
     }
 }
